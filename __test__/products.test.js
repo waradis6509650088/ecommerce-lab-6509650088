88ae812844315a2c636d4f067074def1f54ec171
polyowo <waradis.kam@dome.tu.ac.th>
2024-10-15 23:42:15 +0700

    add GET test cases

--- __test__/products.test.js
+++ __test__/products.test.js
@@ -1,5 +1,25 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('test name', () => {
+describe('GET', () => {
+    test('/products => should return all products', async () => {
+        const res = await request(app).get('/products');
+        const json = await res.body;
+        expect(res.statusCode).toBe(200);
+        expect(json.length).toBe(2);
+    });
+    test('/products/: => should return a product by ID', async () => {
+        const res = await request(app).get('/products/1');
+        const json = await res.body;
+        expect(res.statusCode).toBe(200);
+        expect(json.id).toBe(1);
+        expect(Object.keys(json).length).toBe(4);//single json respond will have only one entry and act as the whole body itself
+    });
+    test('/products/: => should return rescode 404 if not found', async () => {
+        const res = await request(app).get('/products/999');
+        expect(res.statusCode).toBe(404);
+    });
 });
+describe('POST', () => {
+});
+
