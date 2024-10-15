f0ea7be583a444533e82ae60ea2035e38e0fa3f4
waradis6509650088 <waradis.kam@dome.tu.ac.th>
2024-10-16 00:13:10 +0700

    add GET and POST test cases

--- __test__/products.test.js
+++ __test__/products.test.js
@@ -8,18 +8,46 @@ describe('GET', () => {
         expect(res.statusCode).toBe(200);
         expect(json.length).toBe(2);
     });
-    test('/products/: => should return a product by ID', async () => {
+    test('/products/:id => should return a product by ID', async () => {
         const res = await request(app).get('/products/1');
         const json = await res.body;
         expect(res.statusCode).toBe(200);
         expect(json.id).toBe(1);
-        expect(Object.keys(json).length).toBe(4);//single json respond will have only one entry and act as the whole body itself
+        expect(Object.keys(json).length).toBe(4);
+        expect(res.body).toHaveProperty('id','name','price','stock');
     });
-    test('/products/: => should return rescode 404 if not found', async () => {
+    test('/products/:id => should return rescode 404 if not found', async () => {
         const res = await request(app).get('/products/999');
         expect(res.statusCode).toBe(404);
     });
 });
 describe('POST', () => {
+
+    const postInfo = {
+        id: 3,
+        name: 'new product',
+        price: 999,
+        stock: 10
+    }
+
+    test('/products => should add a new products', async () => {
+        const res = await request(app)
+            .post('/products')
+            .send(postInfo)
+            .set('ContentType','application/json');
+        const json = res.body;
+        expect(res.statusCode).toBe(201);
+        expect(Object.keys(json).length).toBe(4);
+        expect(json).toHaveProperty('id','name','price','stock');
+        expect(JSON.stringify(json)).toBe(JSON.stringify(postInfo));
+    });
+});
+describe('PUT', () => {
+    it('products/:id => should update an existing product', async () => {
+
+    });
+    it('should return 404 if product not found', async () => {
+
+    });
 });
 
